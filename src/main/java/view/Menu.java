3cb581d80d3996fd7e16d44787caed29de6bbd74
callegas <callegas.f@gmail.com>
2017-04-27 13:57:44 -0300

    Create customer controller

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -1,8 +1,6 @@
 package view;
 
-import DAO.DAOCustomerImplements;
-import model.Address;
-import model.Customer;
+import controller.CustomerController;
 
 import java.util.Scanner;
 
@@ -29,7 +27,8 @@ public class Menu {
 
                 }
                 case 1: {
-                    register();
+                    CustomerController customerController = new CustomerController();
+                    customerController.registerNewCustomer();
                     break;
                 }
                 case 4: {
@@ -42,38 +41,6 @@ public class Menu {
         }
     }
 
-    public void register() {
-        System.out.println("R E G I S T E R  N E W  C U S T O M E R");
-
-        Customer customer = new Customer();
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter the name: ");
-        customer.setName(input.next());
-        System.out.print("Enter the cpf: ");
-        customer.setCpf(input.next());
-        System.out.print("Enter the email: ");
-        customer.setEmail(input.next());
-        System.out.print("Enter the telephone: ");
-        customer.setTelephone(input.next());
-
-        Address address = new Address();
-        System.out.print("Enter your zip code: ");
-        address.setZip(input.next());
-        System.out.print("Enter the State: ");
-        address.setState(input.next());
-        System.out.print("Enter the City: ");
-        address.setCity(input.next());
-        System.out.print("Enter the street: ");
-        address.setStreet(input.next());
-        System.out.print("Enter the number: ");
-        address.setNumber(input.nextInt());
-        customer.setAddress(address);
-
-
-        DAOCustomerImplements daoCustomerImplements = new DAOCustomerImplements();
-        daoCustomerImplements.save(customer);
-    }
 
 
 }
