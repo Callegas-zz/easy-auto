e8e546bdd33377d8eabb8e5ea7cc6a5f0a8fa7ca
callegas <callegas.f@gmail.com>
2017-05-03 14:29:55 -0300

    fix car simple first

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,6 +1,9 @@
 package model;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
-public abstract class Car implements CarInterface {
+@Entity
+public class Car {
     private String licencePlate;
     private String manufacturer;
     private String model;
@@ -9,14 +12,159 @@ public abstract class Car implements CarInterface {
     private byte gearBox;
     private byte doors;
     private byte places;
+    private boolean oil;
+    private String category;
+    private boolean available;
+    private Double rentPrice;
     private boolean airbag;
     private boolean armored;
     private boolean abs;
     private boolean steering;
     private boolean air;
-    private boolean oil;
     private String seat;
 
-    private boolean available;
-    private Double rentPrice;
+    @Id
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+
+    public void setLicencePlate(String licencePlate) {
+        this.licencePlate = licencePlate;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Double getMotor() {
+        return motor;
+    }
+
+    public void setMotor(Double motor) {
+        this.motor = motor;
+    }
+
+    public byte getGearBox() {
+        return gearBox;
+    }
+
+    public void setGearBox(byte gearBox) {
+        this.gearBox = gearBox;
+    }
+
+    public byte getDoors() {
+        return doors;
+    }
+
+    public void setDoors(byte doors) {
+        this.doors = doors;
+    }
+
+    public byte getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(byte places) {
+        this.places = places;
+    }
+
+    public boolean isOil() {
+        return oil;
+    }
+
+    public void setOil(boolean oil) {
+        this.oil = oil;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public Double getRentPrice() {
+        return rentPrice;
+    }
+
+    public void setRentPrice(Double rentPrice) {
+        this.rentPrice = rentPrice;
+    }
+
+    public boolean isAirbag() {
+        return airbag;
+    }
+
+    public void setAirbag(boolean airbag) {
+        this.airbag = airbag;
+    }
+
+    public boolean isArmored() {
+        return armored;
+    }
+
+    public void setArmored(boolean armored) {
+        this.armored = armored;
+    }
+
+    public boolean isAbs() {
+        return abs;
+    }
+
+    public void setAbs(boolean abs) {
+        this.abs = abs;
+    }
+
+    public boolean isSteering() {
+        return steering;
+    }
+
+    public void setSteering(boolean steering) {
+        this.steering = steering;
+    }
+
+    public boolean isAir() {
+        return air;
+    }
+
+    public void setAir(boolean air) {
+        this.air = air;
+    }
+
+    public String getSeat() {
+        return seat;
+    }
+
+    public void setSeat(String seat) {
+        this.seat = seat;
+    }
 }
